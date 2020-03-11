15cafa3293e969fa75703583dddc866318ec0beb
Raphaël <raphaelpalacio@hotmail.fr>
2020-03-11 14:22:09 +0100

    Update Tableau Graphique.py

--- Tableau Graphique.py
+++ Tableau Graphique.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 
 #verticales
+'''
+for i in range (0,80,10):
     x=[]
     y=[]
     x.append(i)
@@ -11,5 +13,8 @@ import matplotlib.pyplot as plt
 '''
 for i in range (0,80,10):
     plt.plot([i,i],[0,80],'b')
+
+#tableau de meme longueur
+plt.plot(x,y)
 plt.grid()
 plt.show()
